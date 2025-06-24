162f6f55ea6af7f41f886aa9f21ae01b9cb41501
ouissal <bahloulouissalnoor@gmail.com>
2025-06-24 22:10:47 +0100

    TP111

---
+++ tp111.py
@@ -0,0 +1,17 @@
+for i in range(50 ) :
+   
+    if (i%3==0) :
+        print ("fizz")
+
+    elif (i%5==0) :
+        print ("buzz")
+
+    elif(i%3==0)  and (i%5==0) :
+         print ("fizz buzz")
+
+    else :
+        print (i)
+        
+    
+
+print ("end ")
