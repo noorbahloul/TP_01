c6a97e487d94764da970157356eadaaedb450d87
ouissal <bahloulouissalnoor@gmail.com>
2025-06-24 22:30:54 +0100

    TP011

--- tp111.py
+++ tp111.py
@@ -1,13 +1,13 @@
 for i in range(50 ) :
-   
-    if (i%3==0) :
-        print ("fizz")
+    
+    if (i%3==0)  and (i%5==0) :
+         print ("fizz buzz")
 
     elif (i%5==0) :
         print ("buzz")
 
-    elif(i%3==0)  and (i%5==0) :
-         print ("fizz buzz")
+    elif (i%3==0) :
+        print ("fizz")
 
     else :
         print (i)
